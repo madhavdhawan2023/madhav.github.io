fc039df720bac80d88af0342ff220e76c0d03b73
madhavdhawan2023 <124701994+madhavdhawan2023@users.noreply.github.com>
2024-06-30 19:29:46 +0100

    Update themechange.js

--- themechange.js
+++ themechange.js
@@ -1,31 +1,41 @@
-function toggleTheme() {
+var isNightMode = localStorage.getItem('isNightMode') === 'true';
+
+function toggleTheme(isInitializing = false) {
     var toggleBtn = document.getElementById("toggle-theme");
     var body = document.body;
     var navbar = document.querySelector('.navbar');
-    var svgIcon = document.querySelector('.navbar-brand img'); // Target the SVG icon
+    var svgIcon = document.querySelector('.navbar-brand img');
     var fullscreenbtn = document.querySelector('.btn-fss');
+    var para = document.querySelector('.mijhotext');
 
-    if (isNightMode) {
-        // Switch to day mode
-        body.style.backgroundColor = "white";
-        body.style.color = "black";
-        navbar.style.backgroundColor = "#3b3a3a"; // Change navbar background color
-        toggleBtn.classList.remove("night");
-        toggleBtn.innerHTML = '<i class="fas fa-moon"></i>';
-        svgIcon.src = "https://cdn2.steamgriddb.com/icon_thumb/b21f9f98829dea9a48fd8aaddc1f159d.png"; 
-        fullscreenbtn.style.backgroundColor= "black";
-        fullscreenbtn.style.color= "white"
-    } else {
-        // Switch to night mode
-        body.style.backgroundColor = "#3b3a3a";
-        body.style.color = "white";
-        navbar.style.backgroundColor = "#660000"; // Change navbar background color
-        toggleBtn.classList.add("night");
-        toggleBtn.innerHTML = '<i class="fas fa-sun"></i>';
-        svgIcon.src = "./blackicon.svg"; // Change SVG icon for night mode
-        fullscreenbtn.style.backgroundColor= "white";
-        fullscreenbtn.style.color= "black";
+    if (!isInitializing) {
+        isNightMode = !isNightMode; // Toggle the theme mode only if not initializing
+        localStorage.setItem('isNightMode', isNightMode); // Store the theme mode
     }
 
-    isNightMode = !isNightMode; // Toggle the theme mode
+    if (isNightMode) {
+        // Apply night mode styles
+        body.style.backgroundImage= "url(./images/9.jpg)"
+        body.style.color = "white";
+        navbar.style.backgroundColor = "#660000";
+        toggleBtn.classList.add("night");
+        toggleBtn.innerHTML = '<i class="fas fa-sun"></i>';
+        svgIcon.src = "./blackicon.svg";
+        if (fullscreenbtn) {
+            fullscreenbtn.style.backgroundColor = "white";
+            fullscreenbtn.style.color = "black";
+        }
+    } else {
+        // Apply day mode styles
+        body.style.backgroundImage= "url(./images/10.jpg)"
+        navbar.style.backgroundColor = "#3b3a3a";
+        toggleBtn.classList.remove("night");
+        para.style.color= "white";
+        toggleBtn.innerHTML = '<i class="fas fa-moon"></i>';
+        svgIcon.src = "https://cdn2.steamgriddb.com/icon_thumb/b21f9f98829dea9a48fd8aaddc1f159d.png";
+        if (fullscreenbtn) {
+            fullscreenbtn.style.backgroundColor = "black";
+            fullscreenbtn.style.color = "white";
+        }
+    }
 }
