7b15e7bf976f52404a728fd13ea9c77a2d262bf3
madhavdhawan2023 <124701994+madhavdhawan2023@users.noreply.github.com>
2024-06-30 20:02:36 +0100

    Update themechange.js

--- themechange.js
+++ themechange.js
@@ -1,12 +1,4 @@
-// Check if 'isNightMode' is stored in localStorage; default to true if not
-var isNightMode = localStorage.getItem('isNightMode');
-if (isNightMode === null) {
-    isNightMode = true;
-    localStorage.setItem('isNightMode', isNightMode);
-} else {
-    isNightMode = (localStorage.getItem('isNightMode') === 'true');
-}
-
+// Function to toggle the theme
 function toggleTheme(isInitializing = false) {
     var toggleBtn = document.getElementById("toggle-theme");
     var body = document.body;
@@ -18,6 +10,14 @@ function toggleTheme(isInitializing = false) {
     if (!isInitializing) {
         isNightMode = !isNightMode; // Toggle the theme mode only if not initializing
         localStorage.setItem('isNightMode', isNightMode); // Store the theme mode
+    } else {
+        // Check localStorage and set the default to night mode if no preference is found
+        if (localStorage.getItem('isNightMode') === null) {
+            isNightMode = true;
+            localStorage.setItem('isNightMode', isNightMode);
+        } else {
+            isNightMode = localStorage.getItem('isNightMode') === 'true';
+        }
     }
 
     if (isNightMode) {
@@ -35,9 +35,10 @@ function toggleTheme(isInitializing = false) {
     } else {
         // Apply day mode styles
         body.style.backgroundImage = "url(./images/10.jpg)";
+        body.style.color = "black";
         navbar.style.backgroundColor = "#3b3a3a";
         toggleBtn.classList.remove("night");
-        para.style.color = "white";
+        para.style.color = "black";
         toggleBtn.innerHTML = '<i class="fas fa-moon"></i>';
         svgIcon.src = "https://cdn2.steamgriddb.com/icon_thumb/b21f9f98829dea9a48fd8aaddc1f159d.png";
         if (fullscreenbtn) {
