d323986ed68b0ed53ccb38619e485a4b5abda7e2
madhavdhawan2023 <124701994+madhavdhawan2023@users.noreply.github.com>
2024-07-04 22:01:04 +0100

    Update video.js

--- video.js
+++ video.js
@@ -5,7 +5,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
 var startTime = 12;
-var endTime = 10145;
+var endTime = 11850;
 var isNightMode = true;
 
 function onYouTubeIframeAPIReady() {
